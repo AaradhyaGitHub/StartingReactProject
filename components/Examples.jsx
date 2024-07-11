f09137fd2b0faa7660fe9ebaeae32e38726daaad
Aaradhya Poudyal <aaradhyapoudyal@Aaradhyas-MacBook-Air.local>
2024-07-10 22:37:58 -0700

    deeper react dive

--- components/Examples.jsx
+++ components/Examples.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 //anything starting with use is a React Hook
 import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 import { EXAMPLES } from '../data.js';
 
 export default function Examples(){
@@ -18,14 +20,22 @@ export default function Examples(){
   
 
     return(
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected = {selectedTopic === 'components'}onSelect={() =>handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected = {selectedTopic === 'jsx'}onSelect={() =>handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected = {selectedTopic === 'props'}onSelect={() =>handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected = {selectedTopic === 'state'}onSelect={() =>handleSelect('state')}>State</TabButton>
-          </menu>
+        <Section title = "Examples" id="examples">
+          <Tabs
+
+            buttons = {
+
+              <>
+                 <TabButton isSelected = {selectedTopic === 'components'}onClick={() =>handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected = {selectedTopic === 'jsx'}onClick={() =>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected = {selectedTopic === 'props'}onClick={() =>handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected = {selectedTopic === 'state'}onClick={() =>handleSelect('state')}>State</TabButton>
+              </>
+           
+            }
+          
+          
+          >
           {!selectedTopic ? (
             <p>Please select a topic.</p> 
           ) : (
@@ -39,7 +49,12 @@ export default function Examples(){
             </pre>
           </div>
           )}
+          </Tabs>
+          <menu>
+            
+          </menu>
+        
           
-        </section>
+        </Section>
     );
 };
